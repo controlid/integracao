4e4517952869f597ba1a1938a46fdf0d7c912098
Douglas Miguel <douglas.miguel@controlid.com.br>
2019-06-28 18:54:49 -0300

    comeco de exemplo do modo online em node

---
+++ servidor/node/event.js
@@ -0,0 +1,20 @@
+const paths = [
+    '/new_biometric_image.fcgi',
+    '/new_biometric_template.fcgi',
+    '/new_card.fcgi',
+    '/new_user_id_and_password.fcgi',
+    '/new_user_identified.fcgi',
+    '/user_get_image.fcgi',
+    '/device_is_alive.fcgi'
+];
+
+module.exports = (url, request, response) => {
+
+    if (!paths.includes(url.pathname)) {
+        return;
+    }
+
+    console.log('Event pathname -> ' + url.pathname);
+    console.log('Event query -> ' + JSON.stringify(url.query));
+
+}
