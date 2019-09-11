15162407bd3e2f85dd0b9621ebc4dec30322d6ef
Douglas Miguel <douglas.miguel@controlid.com.br>
2019-09-11 12:56:44 -0300

    servidor (springboot): adicionado anotações @RequestParam

--- servidor/springboot/src/main/java/br/com/controlid/springboot/api/EventAPI.java
+++ servidor/springboot/src/main/java/br/com/controlid/springboot/api/EventAPI.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -13,7 +14,7 @@ import java.util.Map;
 public class EventAPI {
 
 	@PostMapping("/new_biometric_image.fcgi")
-	public ResponseEntity newBiometricImage(@RequestBody byte[] requestBody, Map<String, String> allRequestParams) {
+	public ResponseEntity newBiometricImage(@RequestBody byte[] requestBody, @RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.newBiometricImage");
 		System.out.println("requestBodyLength = [" + ( requestBody != null ? requestBody.length : 0 ) + "], allRequestParams = [" + allRequestParams + "]");
 
@@ -23,7 +24,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/new_biometric_template.fcgi")
-	public ResponseEntity newBiometricTemplate(@RequestBody byte[] requestBody, Map<String, String> allRequestParams) {
+	public ResponseEntity newBiometricTemplate(@RequestBody byte[] requestBody, @RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.newBiometricTemplate");
 		System.out.println("requestBodyLength = [" + ( requestBody != null ? requestBody.length : 0 ) + "], allRequestParams = [" + allRequestParams + "]");
 
@@ -33,7 +34,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/new_card.fcgi")
-	public ResponseEntity newCard(Map<String, String> allRequestParams) {
+	public ResponseEntity newCard(@RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.newCard");
 		System.out.println("allRequestParams = [" + allRequestParams + "]");
 
@@ -43,7 +44,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/new_user_id_and_password.fcgi")
-	public ResponseEntity newUserIdAndPassword(Map<String, String> allRequestParams) {
+	public ResponseEntity newUserIdAndPassword(@RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.newUserIdAndPassword");
 		System.out.println("allRequestParams = [" + allRequestParams + "]");
 
@@ -53,7 +54,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/new_user_identified.fcgi")
-	public ResponseEntity newUserIdentified(Map<String, String> allRequestParams) {
+	public ResponseEntity newUserIdentified(@RequestParam Map<String, String> allRequestParams) {
 		System.out.println("IdentificationAPI.newUserIdentified");
 		System.out.println("allRequestParams = [" + allRequestParams + "]");
 
@@ -63,7 +64,7 @@ public class EventAPI {
 	}
 
 	@GetMapping("/user_get_image.fcgi")
-	public ResponseEntity userGetImage(Map<String, String> allRequestParams) {
+	public ResponseEntity userGetImage(@RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.userGetImage");
 		System.out.println("allRequestParams = [" + allRequestParams + "]");
 
@@ -73,7 +74,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/device_is_alive.fcgi")
-	public ResponseEntity deviceIsAlive(Map<String, String> allRequestParams) {
+	public ResponseEntity deviceIsAlive(@RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.deviceIsAlive");
 		System.out.println("allRequestParams = [" + allRequestParams + "]");
 
@@ -83,7 +84,7 @@ public class EventAPI {
 	}
 
 	@PostMapping("/template_create.fcgi")
-	public ResponseEntity templateCreate(@RequestBody byte[] requestBody, Map<String, String> allRequestParams) {
+	public ResponseEntity templateCreate(@RequestBody byte[] requestBody, @RequestParam Map<String, String> allRequestParams) {
 		System.out.println("EventAPI.templateCreate");
 		System.out.println("requestBodyLength = [" + ( requestBody != null ? requestBody.length : 0 ) + "], allRequestParams = [" + allRequestParams + "]");
 
