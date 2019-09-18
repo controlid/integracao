7532a68f46bc7271c581fbd958534a20f1dbd440
Douglas Miguel <douglas.miguel@controlid.com.br>
2019-09-18 13:17:18 -0300

    servidor (flask): implementado device_is_alive

--- servidor/flask/api/event.py
+++ servidor/flask/api/event.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 event = Blueprint('event', __name__)
 
-@event.route('/device_is_alive.fcgi')
+@event.route('/device_is_alive.fcgi', methods=['POST'])
 def device_is_alive():
+    print('event.device_is_alive')
+    print(request.get_json())
+    print(request.args)
+
     return jsonify(
-        access_logs=1
+        access_logs=request.get_json()['access_logs']
     )
--- servidor/flask/app.py
+++ servidor/flask/app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 app.register_blueprint(event)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
