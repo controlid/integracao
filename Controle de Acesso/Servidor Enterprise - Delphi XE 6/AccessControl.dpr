3b73236fb176103c7793ff4e017209aaf3fba768
Tiago Macedo <tiago.macedo@controlid.com.br>
2020-02-05 11:25:55 -0300

    Incluir exemplo de servidor para o Delphi XE 6.

---
+++ Controle de Acesso/Servidor Enterprise - Delphi XE 6/AccessControl.dpr
@@ -0,0 +1,16 @@
+program AccessControl;
+
+uses
+  Vcl.Forms,
+  UnitAccessControl in 'UnitAccessControl.pas' {FormControleAcesso},
+  UnitHttpClient in 'UnitHttpClient.pas',
+  UnitDevice in 'UnitDevice.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormControleAcesso, FormControleAcesso);
+  Application.Run;
+end.
